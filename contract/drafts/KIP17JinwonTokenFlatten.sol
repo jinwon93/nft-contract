fc37eaf5bfe926834a456318653fbb192f9ec6c7
jinwon93 <kjw8208@nate.com>
2022-09-16 21:29:43 +0900

    20220916

--- contract/drafts/KIP17JinwonTokenFlatten.sol
+++ contract/drafts/KIP17JinwonTokenFlatten.sol
@@ -413,4 +413,29 @@ contract KIP17Metadata is KIP13, KIP17, IKIP17Metadata {
     mapping(uint256 => string) private _tokenURIs;
 
 
+
+     bytes4 private constant _INTERFACE_ID_KIP17_METADATA = 0x5b5e139f;
+
+
+     constructor (string memory name, string memory symbol) public {
+        _name = name;
+        _symbol = symbol;
+
+        
+        _registerInterface(_INTERFACE_ID_KIP17_METADATA);
+    }
+
+     function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        require(_exists(tokenId), "KIP17Metadata: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
+    }
+
 }    
