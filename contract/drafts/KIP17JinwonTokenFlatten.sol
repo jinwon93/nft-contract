b5f5b09560d5e176d6db56f97311adf41eefa7a1
jinwon93 <kjw8208@nate.com>
2022-09-21 21:54:03 +0900

    20220921

--- contract/drafts/KIP17JinwonTokenFlatten.sol
+++ contract/drafts/KIP17JinwonTokenFlatten.sol
@@ -527,3 +527,29 @@ library Roles {
         return role.bearer[account];
     }
 }
+
+
+pragma solidity ^0.5.0;
+
+contract MinterRole {
+    using Roles for Roles.Role;
+
+    event MinterAdded(address indexed account);
+    event MinterRemoved(address indexed account);
+
+    Roles.Role private _minters;
+
+    constructor () internal {
+        _addMinter(msg.sender);
+    }
+
+    modifier onlyMinter() {
+        require(isMinter(msg.sender), "MinterRole: caller does not have the Minter role");
+        _;
+    }
+
+    function isMinter(address account) public view returns (bool) {
+        return _minters.has(account);
+    }
+
+}
