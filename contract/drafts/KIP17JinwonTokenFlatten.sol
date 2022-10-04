7520141a100b884b4c2fb0eb3560c5c7655d6355
jinwon93 <kjw8208@nate.com>
2022-10-04 19:19:32 +0900

    20221004

--- contract/drafts/KIP17JinwonTokenFlatten.sol
+++ contract/drafts/KIP17JinwonTokenFlatten.sol
@@ -864,3 +864,26 @@ contract KIP17Mintable is KIP17, MinterRole {
         return true;
     }
 }
+
+
+pragma solidity ^0.5.0;
+
+
+
+contract KIP17Burnable is KIP13, KIP17 {
+    
+    bytes4 private constant _INTERFACE_ID_KIP17_BURNABLE = 0x42966c68;
+
+    
+    constructor () public {
+        
+        _registerInterface(_INTERFACE_ID_KIP17_BURNABLE);
+    }
+
+    
+    function burn(uint256 tokenId) public {
+        //solhint-disable-next-line max-line-length
+        require(_isApprovedOrOwner(msg.sender, tokenId), "KIP17Burnable: caller is not owner nor approved");
+        _burn(tokenId);
+    }
+}
