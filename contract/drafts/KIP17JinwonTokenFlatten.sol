186d7796619b20d5a04f3874fb06fbafc63b969d
jinwon93 <kjw8208@nate.com>
2022-10-06 19:51:19 +0900

    20221006

--- contract/drafts/KIP17JinwonTokenFlatten.sol
+++ contract/drafts/KIP17JinwonTokenFlatten.sol
@@ -930,3 +930,31 @@ contract PauserRole {
         emit PauserRemoved(account);
     }
 }
+
+
+pragma solidity ^0.5.0;
+
+
+contract Pausable is PauserRole {
+    
+    event Paused(address account);
+
+    
+    event Unpaused(address account);
+
+    bool private _paused;
+
+    
+    constructor () internal {
+        _paused = false;
+    }
+
+    function paused() public view returns (bool) {
+        return _paused;
+    }
+
+    
+    modifier whenNotPaused() {
+        require(!_paused, "Pausable: paused");
+        _;
+    }
