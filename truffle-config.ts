cca9488d9210325cfad97741113831075515eae9
jinwon93 <kjw8208@nate.com>
2022-10-10 20:52:38 +0900

    20221010

---
+++ truffle-config.ts
@@ -0,0 +1,35 @@
+module.exports = {
+
+
+      
+
+  networks: {
+    
+  },
+
+  
+  mocha: {
+    // timeout: 100000
+  },
+
+  
+  compilers: {
+    solc: {
+      version: "0.8.12",    // Fetch exact version from solc-bin (default: truffle's version)
+      
+    }
+  },
+
+  
+  // db: {
+    // enabled: false,
+    // host: "127.0.0.1",
+    // adapter: {
+    //   name: "sqlite",
+    //   settings: {
+    //     directory: ".db"
+    //   }
+    // }
+  // }
+
+}
