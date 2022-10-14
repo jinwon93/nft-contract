0ae996e5ed7d7839596c17904086d374cac52cff
jinwon93 <kjw8208@nate.com>
2022-10-14 21:37:34 +0900

    20221014

---
+++ contracts/IKIP17Receiver.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+
+
+contract IKIP17Receiver {
+    
+    function onKIP17Received(address operator, address from, uint256 tokenId, bytes memory data)
+    public returns (bytes4);
+}
