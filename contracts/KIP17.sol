d28fbccd5206466b4229e187bf7a231d40ccb410
jinwon93 <kjw8208@nate.com>
2022-10-15 21:35:27 +0900

    20221015

---
+++ contracts/KIP17.sol
@@ -0,0 +1,201 @@
+pragma solidity ^0.5.0;
+
+import "./IKIP17.sol";
+import "./IERC721Receiver.sol";
+import "./IKIP17Receiver.sol";
+// import "./math/SafeMath.sol";
+// import "./utils/Address.sol";
+// import "./drafts/Counters.sol";
+// import "./introspection/KIP13.sol";
+
+
+contract KIP17 is KIP13, IKIP17 {
+    using SafeMath for uint256;
+    using Address for address;
+    using Counters for Counters.Counter;
+
+    
+    bytes4 private constant _KIP17_RECEIVED = 0x6745782b;
+
+    
+    bytes4 private constant _ERC721_RECEIVED = 0x150b7a02;
+
+    
+    mapping (uint256 => address) private _tokenOwner;
+
+    
+    mapping (uint256 => address) private _tokenApprovals;
+
+    
+    mapping (address => Counters.Counter) private _ownedTokensCount;
+
+    
+    mapping (address => mapping (address => bool)) private _operatorApprovals;
+
+   
+    bytes4 private constant _INTERFACE_ID_KIP17 = 0x80ac58cd;
+
+    constructor () public {
+        // register the supported interfaces to conform to KIP17 via KIP13
+        _registerInterface(_INTERFACE_ID_KIP17);
+    }
+
+    
+    function balanceOf(address owner) public view returns (uint256) {
+        require(owner != address(0), "KIP17: balance query for the zero address");
+
+        return _ownedTokensCount[owner].current();
+    }
+
+    
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address owner = _tokenOwner[tokenId];
+        require(owner != address(0), "KIP17: owner query for nonexistent token");
+
+        return owner;
+    }
+
+   
+    function approve(address to, uint256 tokenId) public {
+        address owner = ownerOf(tokenId);
+        require(to != owner, "KIP17: approval to current owner");
+
+        require(msg.sender == owner || isApprovedForAll(owner, msg.sender),
+            "KIP17: approve caller is not owner nor approved for all"
+        );
+
+        _tokenApprovals[tokenId] = to;
+        emit Approval(owner, to, tokenId);
+    }
+
+    
+    function getApproved(uint256 tokenId) public view returns (address) {
+        require(_exists(tokenId), "KIP17: approved query for nonexistent token");
+
+        return _tokenApprovals[tokenId];
+    }
+
+   
+    function setApprovalForAll(address to, bool approved) public {
+        require(to != msg.sender, "KIP17: approve to caller");
+
+        _operatorApprovals[msg.sender][to] = approved;
+        emit ApprovalForAll(msg.sender, to, approved);
+    }
+
+  
+    function isApprovedForAll(address owner, address operator) public view returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+
+   
+    function transferFrom(address from, address to, uint256 tokenId) public {
+        //solhint-disable-next-line max-line-length
+        require(_isApprovedOrOwner(msg.sender, tokenId), "KIP17: transfer caller is not owner nor approved");
+
+        _transferFrom(from, to, tokenId);
+    }
+
+    
+    function safeTransferFrom(address from, address to, uint256 tokenId) public {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public {
+        transferFrom(from, to, tokenId);
+        require(_checkOnKIP17Received(from, to, tokenId, _data), "KIP17: transfer to non KIP17Receiver implementer");
+    }
+
+    
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
+    }
+
+   
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
+        require(_exists(tokenId), "KIP17: operator query for nonexistent token");
+        address owner = ownerOf(tokenId);
+        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+    }
+
+    
+    function _mint(address to, uint256 tokenId) internal {
+        require(to != address(0), "KIP17: mint to the zero address");
+        require(!_exists(tokenId), "KIP17: token already minted");
+
+        _tokenOwner[tokenId] = to;
+        _ownedTokensCount[to].increment();
+
+        emit Transfer(address(0), to, tokenId);
+    }
+
+   
+    function _burn(address owner, uint256 tokenId) internal {
+        require(ownerOf(tokenId) == owner, "KIP17: burn of token that is not own");
+
+        _clearApproval(tokenId);
+
+        _ownedTokensCount[owner].decrement();
+        _tokenOwner[tokenId] = address(0);
+
+        emit Transfer(owner, address(0), tokenId);
+    }
+
+    
+    function _burn(uint256 tokenId) internal {
+        _burn(ownerOf(tokenId), tokenId);
+    }
+
+    
+    function _transferFrom(address from, address to, uint256 tokenId) internal {
+        require(ownerOf(tokenId) == from, "KIP17: transfer of token that is not own");
+        require(to != address(0), "KIP17: transfer to the zero address");
+
+        _clearApproval(tokenId);
+
+        _ownedTokensCount[from].decrement();
+        _ownedTokensCount[to].increment();
+
+        _tokenOwner[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    
+    function _checkOnKIP17Received(address from, address to, uint256 tokenId, bytes memory _data)
+        internal returns (bool)
+    {
+        bool success; 
+        bytes memory returndata;
+
+        if (!to.isContract()) {
+            return true;
+        }
+
+        // Logic for compatibility with ERC721.
+        (success, returndata) = to.call(
+            abi.encodeWithSelector(_ERC721_RECEIVED, msg.sender, from, tokenId, _data)
+        );
+        if (returndata.length != 0 && abi.decode(returndata, (bytes4)) == _ERC721_RECEIVED) {
+            return true;
+        }
+
+        (success, returndata) = to.call(
+            abi.encodeWithSelector(_KIP17_RECEIVED, msg.sender, from, tokenId, _data)
+        );
+        if (returndata.length != 0 && abi.decode(returndata, (bytes4)) == _KIP17_RECEIVED) {
+            return true;
+        }
+
+        return false;
+    }
+
+    
+    function _clearApproval(uint256 tokenId) private {
+        if (_tokenApprovals[tokenId] != address(0)) {
+            _tokenApprovals[tokenId] = address(0);
+        }
+    }
+}
