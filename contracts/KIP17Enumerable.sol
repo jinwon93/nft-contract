6bc932310fe0d927802da2dcb6472f1fa81bc688
jinwon93 <kjw8208@nate.com>
2022-10-18 20:01:12 +0900

    20221018

---
+++ contracts/KIP17Enumerable.sol
@@ -0,0 +1,132 @@
+pragma solidity ^0.5.0;
+
+import "./IKIP17Enumerable.sol";
+import "./KIP17.sol";
+// import "./introspection/KIP13.sol";
+
+
+
+contract KIP17Enumerable is KIP13, KIP17, IKIP17Enumerable {
+    
+    mapping(address => uint256[]) private _ownedTokens;
+
+
+    mapping(uint256 => uint256) private _ownedTokensIndex;
+
+    
+    uint256[] private _allTokens;
+
+  
+    mapping(uint256 => uint256) private _allTokensIndex;
+
+    
+    bytes4 private constant _INTERFACE_ID_KIP17_ENUMERABLE = 0x780e9d63;
+
+    
+    constructor () public {
+        
+        _registerInterface(_INTERFACE_ID_KIP17_ENUMERABLE);
+    }
+
+   
+    function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256) {
+        require(index < balanceOf(owner), "KIP17Enumerable: owner index out of bounds");
+        return _ownedTokens[owner][index];
+    }
+
+    
+    function totalSupply() public view returns (uint256) {
+        return _allTokens.length;
+    }
+
+    
+    function tokenByIndex(uint256 index) public view returns (uint256) {
+        require(index < totalSupply(), "KIP17Enumerable: global index out of bounds");
+        return _allTokens[index];
+    }
+
+    
+    function _transferFrom(address from, address to, uint256 tokenId) internal {
+        super._transferFrom(from, to, tokenId);
+
+        _removeTokenFromOwnerEnumeration(from, tokenId);
+
+        _addTokenToOwnerEnumeration(to, tokenId);
+    }
+
+    
+    function _mint(address to, uint256 tokenId) internal {
+        super._mint(to, tokenId);
+
+        _addTokenToOwnerEnumeration(to, tokenId);
+
+        _addTokenToAllTokensEnumeration(tokenId);
+    }
+
+    
+    function _burn(address owner, uint256 tokenId) internal {
+        super._burn(owner, tokenId);
+
+        _removeTokenFromOwnerEnumeration(owner, tokenId);
+        
+        _ownedTokensIndex[tokenId] = 0;
+
+        _removeTokenFromAllTokensEnumeration(tokenId);
+    }
+
+    
+    function _tokensOfOwner(address owner) internal view returns (uint256[] storage) {
+        return _ownedTokens[owner];
+    }
+
+    
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
+
+    
+    function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
+    }
+
+    
+    function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId) private {
+        
+
+        uint256 lastTokenIndex = _ownedTokens[from].length.sub(1);
+        uint256 tokenIndex = _ownedTokensIndex[tokenId];
+
+        
+        if (tokenIndex != lastTokenIndex) {
+            uint256 lastTokenId = _ownedTokens[from][lastTokenIndex];
+
+            _ownedTokens[from][tokenIndex] = lastTokenId; 
+            _ownedTokensIndex[lastTokenId] = tokenIndex; 
+        }
+
+        
+        _ownedTokens[from].length--;
+
+        
+    }
+
+    
+    function _removeTokenFromAllTokensEnumeration(uint256 tokenId) private {
+        
+
+        uint256 lastTokenIndex = _allTokens.length.sub(1);
+        uint256 tokenIndex = _allTokensIndex[tokenId];
+
+        
+        uint256 lastTokenId = _allTokens[lastTokenIndex];
+
+        _allTokens[tokenIndex] = lastTokenId; 
+        _allTokensIndex[lastTokenId] = tokenIndex; 
+
+        \
+        _allTokens.length--;
+        _allTokensIndex[tokenId] = 0;
+    }
+}
