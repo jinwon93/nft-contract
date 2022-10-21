8b74c1b8ee2d41956b347662262b99d0e73e6cdc
jinwon93 <kjw8208@nate.com>
2022-10-21 19:23:31 +0900

    20221021

---
+++ contracts/KIP17Metadata.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.5.0;
+
+import "./KIP17.sol";
+import "./IKIP17Metadata.sol";
+// import "./introspection/KIP13.sol";
+
+contract KIP17Metadata is KIP13, KIP17, IKIP17Metadata {
+    
+    string private _name;
+
+    
+    string private _symbol;
+
+    
+    mapping(uint256 => string) private _tokenURIs;
+
+    
+    bytes4 private constant _INTERFACE_ID_KIP17_METADATA = 0x5b5e139f;
+
+    
+    constructor (string memory name, string memory symbol) public {
+        _name = name;
+        _symbol = symbol;
+
+        
+        _registerInterface(_INTERFACE_ID_KIP17_METADATA);
+    }
+
+    
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        require(_exists(tokenId), "KIP17Metadata: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
+    }
+
+    
+    function _setTokenURI(uint256 tokenId, string memory uri) internal {
+        require(_exists(tokenId), "KIP17Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = uri;
+    }
+
+    
+    function _burn(address owner, uint256 tokenId) internal {
+        super._burn(owner, tokenId);
+
+        
+        if (bytes(_tokenURIs[tokenId]).length != 0) {
+            delete _tokenURIs[tokenId];
+        }
+    }
+}
